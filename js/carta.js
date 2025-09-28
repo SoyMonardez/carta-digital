ab4fa62ce27de8da23a31d9d4dd2b74bb8e5f89c
SoyMonardez <monardezcode@gmail.com>
2025-09-28 01:47:44 -0300

    carta digital v3
    se añadieron los "ver todos" de cada catalogo, se le elimino el buscardo junto con el boton con el icono de filtrar, en su lugar se le añadieorn mas fotones para filtrar por catalogo, se le pusieron las imagenes y se le añadio un icono alado del h1 junto con un favicon

--- js/carta.js
+++ js/carta.js
@@ -4,3 +4,26 @@ const fabToggle = document.querySelector('.social-fab-toggle');
 fabToggle.addEventListener('click', () => {
     fabContainer.classList.toggle('active');
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const buttons = document.querySelectorAll(".categories button");
+  const sections = document.querySelectorAll(".food-section");
+
+  buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+      // Quitar 'active' de todos los botones
+      buttons.forEach(b => b.classList.remove("active"));
+      btn.classList.add("active");
+
+      const filter = btn.getAttribute("data-filter");
+
+      sections.forEach(section => {
+        if (filter === "all" || section.dataset.category === filter) {
+          section.style.display = "block";
+        } else {
+          section.style.display = "none";
+        }
+      });
+    });
+  });
+});
